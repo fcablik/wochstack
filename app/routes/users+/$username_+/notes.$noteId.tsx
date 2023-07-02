ce4c2dff9c30f64f6ab60a4ef5b317fed3fd5f64
fcablik <filip.cablik@icloud.com>
2023-07-02 20:23:35 +0200

    convo of 'remix' and 'epicstack' tests

--- app/routes/users+/$username_+/notes.$noteId.tsx
+++ app/routes/users+/$username_+/notes.$noteId.tsx
@@ -1,9 +1,10 @@
 import { json, type DataFunctionArgs } from '@remix-run/node'
-import { Link, useLoaderData } from '@remix-run/react'
+import { Link, useFetcher, useLoaderData } from '@remix-run/react'
+import invariant from 'tiny-invariant'
 import { GeneralErrorBoundary } from '~/components/error-boundary.tsx'
 import { Button } from '~/components/ui/button.tsx'
 import { DeleteNote } from '~/routes/resources+/delete-note.tsx'
-import { getUserId } from '~/utils/auth.server.ts'
+import { getUserId, requireUserId } from '~/utils/auth.server.ts'
 import { prisma } from '~/utils/db.server.ts'
 
 export async function loader({ request, params }: DataFunctionArgs) {
@@ -19,14 +20,56 @@ export async function loader({ request, params }: DataFunctionArgs) {
 			ownerId: true,
 		},
 	})
+
+	const favorite = userId 
+		? await prisma.favorite.findFirst({
+			where: {
+				ownerId: userId,
+				noteId: params.noteId,
+			},
+			select: { id: true },
+		})
+	 : null
 	if (!note) {
 		throw new Response('Not found', { status: 404 })
 	}
-	return json({ note, isOwner: userId === note.ownerId })
+	return json({
+		note, 
+		isFavorited : Boolean(favorite),
+		isOwner: userId === note.ownerId,
+	})
+}
+
+export async function action({ request, params } : DataFunctionArgs) {
+	const userId = await requireUserId(request)
+	invariant(params.noteId, 'noteId is required')
+	const form = await request.formData()
+	const isFavorited = form.get('isFavorited') === 'true'
+
+	if (isFavorited) {
+		await prisma.favorite.create({
+			select: { id: true },
+			data: {
+				ownerId: userId,
+				noteId: params.noteId,
+			},
+		})
+	} else {
+		await prisma.favorite.deleteMany({
+			where: {
+				ownerId: userId,
+				noteId: params.noteId,
+			},
+		})
+	}
+	return json({ status: 'success' })
 }
 
 export default function NoteRoute() {
 	const data = useLoaderData<typeof loader>()
+	const favoriteFetcher = useFetcher()
+	const pendingFavorite = favoriteFetcher.state !== 'idle'
+	const isFavorite = pendingFavorite ? favoriteFetcher.formData?.get('isFavorited') === 'true' : data.isFavorited 	
 
 	return (
 		<div className="flex h-full flex-col">
@@ -34,14 +77,24 @@ export default function NoteRoute() {
 				<h2 className="mb-2 text-h2 lg:mb-6">{data.note.title}</h2>
 				<p className="text-sm md:text-lg">{data.note.content}</p>
 			</div>
-			{data.isOwner ? (
-				<div className="flex justify-end gap-4">
-					<DeleteNote id={data.note.id} />
-					<Button asChild>
-						<Link to="edit">Edit</Link>
-					</Button>
-				</div>
-			) : null}
+			<div className="flex justify-end gap-4">
+				<favoriteFetcher.Form method="POST">
+					<input 
+						type="hidden"
+						name="isFavorited"
+						value={(!isFavorite).toString()}
+					/>
+					<button type="submit">{isFavorite ? '❤️': '🖤'}</button>
+				</favoriteFetcher.Form>
+				{data.isOwner ? (
+					<>
+						<DeleteNote id={data.note.id} />
+						<Button asChild>
+							<Link to="edit">Edit</Link>
+						</Button>
+					</>
+				) : null}
+			</div>
 		</div>
 	)
 }
